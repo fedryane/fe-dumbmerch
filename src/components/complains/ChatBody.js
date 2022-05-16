883cc4fb783dedd8f47f82bac8c7eaf8a9ad700e
Fedryan1337 <bandarisafedryan@gmail.com>
2022-05-17 05:14:41 +0700

    commit

--- src/components/complains/ChatBody.js
+++ src/components/complains/ChatBody.js
@@ -9,7 +9,7 @@ export default function Chat({ contact, user, messages, sendMessage }) {
           <div id="chat-messages" style={{ height: "80vh" }} className="overflow-auto px-3 py-2">
             {messages.map((item, index) => (
               <div key={index}>
-                <div className={`d-flex py-1 ${item.idSender === user.id ? "justify-content-end text-white" : "justify-content-start"}`}>
+                <div className={`d-flex py-1 text-white ${item.idSender === user.id ? "justify-content-end text-white" : "justify-content-start"}`}>
                   {item.idSender !== user.id && <img src={contact.profile?.image || default_profile} className="rounded-circle me-2 img-chat" alt="bubble avatar" />}
                   <div className={item.idSender === user.id ? "chat-me" : "chat-other"}>{item.message}</div>
                 </div>
