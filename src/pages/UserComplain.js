6873876d2aa33d3d4106bd8999d6c2066211dd7a
Fedryan1337 <bandarisafedryan@gmail.com>
2022-05-17 05:10:17 +0700

    commit

--- src/pages/UserComplain.js
+++ src/pages/UserComplain.js
@@ -4,6 +4,7 @@ import { io } from "socket.io-client";
 import ChatList from "../components/complains/ChatList";
 import ChatBody from "../components/complains/ChatBody";
 import { UserContext } from "../context/userContext";
+import { Col, Container, Row } from "react-bootstrap";
 
 // initial variable outside component
 let socket;
@@ -100,19 +101,19 @@ function UserComplain() {
 
   console.log(messages);
   return (
-    <div>
+    <>
       <NavbarUser />
-      <div className="container">
-        <div className="row">
-          <div className="col">
+      <Container fluid style={{ height: "89.5vh" }}>
+        <Row>
+          <Col md={3} style={{ height: "89.5vh" }} className="px-3 border-end border-dark overflow-auto">
             <ChatList dataContact={contacts} clickContact={onClickContact} contact={contact} />
-          </div>
-          <div className="col">
+          </Col>
+          <Col md={9} style={{ maxHeight: "89.5vh" }} className="px-0">
             <ChatBody contact={contact} messages={messages} user={state.user} sendMessage={onSendMessage} />
-          </div>
-        </div>
-      </div>
-    </div>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 }
 
