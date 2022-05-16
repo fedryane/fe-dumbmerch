b2f40af9faad71814f2a441ff4bafa4aaad4958d
Fedryan1337 <bandarisafedryan@gmail.com>
2022-05-17 04:35:47 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ import "../src/App.css";
 import Login from "./pages/Login";
 // import Category from "./components/pages/Category";
 import Register from "./pages/Register";
-import Navbar from "./components/navbar/Navbar"
+import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/Home";
 import DetailProduct from "./pages/DetailProduct";
 import Product from "./pages/Product";
 import EditCategory from "./components/category/EditCategory";
 import EditProduct from "./components/product/EditProduct";
-import ProductList from "./components/product/ProductList"
-import CategoryList from "./components/category/CategoryList"
+import ProductList from "./components/product/ProductList";
+import CategoryList from "./components/category/CategoryList";
 // import EditProductProps from "./components/inc/EditProductProps";
 import Profile from "./pages/Profile";
 import AdminComplain from "./pages/AdminComplain";
@@ -22,7 +22,7 @@ import UserComplain from "./pages/UserComplain";
 // import ErrorPage from "./components/pages/ErrorPage";
 import AddCategory from "./components/category/AddCategory";
 import AddProduct from "./components/product/AddProduct";
-import Category from "./pages/Category"
+import Category from "./pages/Category";
 
 // Init token on axios every time the app is refreshed here ...
 import { setAuthToken, API } from "../src/config/api";
@@ -43,7 +43,7 @@ function App() {
       navigate("/login");
     } else {
       if (state.user.status === "admin") {
-        navigate("/user-complain");
+        navigate("/admin-complain");
       } else if (state.user.status === "customer") {
         navigate("/home");
       }
@@ -98,28 +98,27 @@ function App() {
     // <AddProduct/>
     // </div>
     <Routes>
-    <Route path="/login" element={<Login />} />
-    <Route path="/register" element={<Register />} />
-    <Route path="/login" element={<Register />} />
-    <Route path="/" element={<Login />} />
-    <Route path="/home" element={<Home />} />
-    
-    <Route path="/product" element={<Product />} />
-    <Route path="/profile" element={<Profile />} />
-    <Route path="/admin-complain" element={<AdminComplain />} />
-    <Route path="/user-complain" element={<UserComplain />} />
-    
-    <Route path="/category" element={<Category />} />
-    <Route path="/edit-category/:id" element={<EditCategory />} />
-    <Route path="/add-category" element={<AddCategory />} />
-    <Route path="/add-product" element={<AddProduct />} />
-    <Route path="/edit-product/:id" element={<EditProduct />} />
-    <Route path="/detail-product/:id" element={<DetailProduct />} />
-    <Route path="/profile" element={<Profile />} />
-    </Routes>
-    );
-  }
-  // <Route path="*" element={<ErrorPage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Register />} />
+      <Route path="/" element={<Login />} />
+      <Route path="/home" element={<Home />} />
 
+      <Route path="/product" element={<Product />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/admin-complain" element={<AdminComplain />} />
+      <Route path="/user-complain" element={<UserComplain />} />
+
+      <Route path="/category" element={<Category />} />
+      <Route path="/edit-category/:id" element={<EditCategory />} />
+      <Route path="/add-category" element={<AddCategory />} />
+      <Route path="/add-product" element={<AddProduct />} />
+      <Route path="/edit-product/:id" element={<EditProduct />} />
+      <Route path="/detail-product/:id" element={<DetailProduct />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
+  );
+}
+// <Route path="*" element={<ErrorPage />} />
 
 export default App;
